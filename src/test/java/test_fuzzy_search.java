44c13fbae75de271e464f585b06347ccf52b3f96
Tay Qi Xiang <tayqixiang@gmail.com>
2021-05-09 15:00:47 +0800

    testing

---
+++ src/test/java/test_fuzzy_search.java
@@ -0,0 +1,7 @@
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+
+public class test_fuzzy_search {
+    public static void main(String[] args) {
+        System.out.println(FuzzySearch.partialRatio("similar", "somewhresimlrbetweenthisstring"));
+    }
+}
