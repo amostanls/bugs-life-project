679333f33ab0e9dac6dc1c3fd9ea0dc5aea54db7
Tay Qi Xiang <tayqixiang@gmail.com>
2021-05-11 14:35:32 +0800

    initial commit for maven

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bugs-life-project
-This is a project for WIA1002
-
-Created by: 
-Qi Xiang, Li Sheng, Wei Lok, and Zhang Xiang
--- src/test/java/test_fuzzy_search.java
+++
@@ -1,7 +0,0 @@
-import me.xdrop.fuzzywuzzy.FuzzySearch;
-
-public class test_fuzzy_search {
-    public static void main(String[] args) {
-        System.out.println(FuzzySearch.partialRatio("similar", "somewhresimlrbetweenthisstring"));
-    }
-}
