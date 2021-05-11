859f3e69218c64655e1ac60d7bfe83c4d0ae9fab
Tay Qi Xiang <tayqixiang@gmail.com>
2021-05-11 18:13:41 +0800

    updated login ui

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,29 +1,47 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class login extends JFrame {
-    private JPanel user;
+
     private JPanel rootPanel;
     private JTextField username_field;
     private JPasswordField password_field;
     private JButton button1;
+    private JPanel title_panel;
+    private JPanel input_panel;
 
-    public login(){
+    public login() {
 
         add(rootPanel);
         setTitle("Bugs life");
-        setSize(500,500);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+        Border panelBorder = BorderFactory.createMatteBorder(0, 0, 0, 5, Color.black);
+        title_panel.setBorder(panelBorder);
+
+        Border titleBorder = BorderFactory.createMatteBorder(0, 0, 4, 0, Color.gray);
+        input_panel.setBorder(titleBorder);
+
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(username_field.equals("admin")&&password_field.equals("password")){
-                    JOptionPane.showMessageDialog(null,"Correct password");
+                String username=username_field.getText().trim();
+                String password=String.valueOf(password_field.getPassword()).trim();
+                if (username.equals("admin") && password.equals("password")) {
+                    JOptionPane.showMessageDialog(null, "Correct password");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Wrong username or password");
                 }
-                else{
-                    JOptionPane.showMessageDialog(null,"wrong password");
-                }
+                username_field.setText("");
+                password_field.setText("");
             }
         });
     }
--- src/main/java/main_class.java
+++ src/main/java/main_class.java
@@ -4,9 +4,9 @@ import javax.swing.*;
 
 public class main_class {
     public static void main(String[] args) {
-        System.out.println("HELLO world");
+
         login loginPage=new login();
-        loginPage.setVisible(true);
+
 
     }
 }
