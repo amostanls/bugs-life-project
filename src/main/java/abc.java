53a2dc80527c6c46b680b1a860eb4c61fa67f8cd
Tay Qi Xiang <tayqixiang@gmail.com>
2021-05-11 14:46:20 +0800

    maven changes

--- src/main/java/abc.java
+++ src/main/java/abc.java
@@ -1,5 +1,8 @@
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+
 public class abc {
     public static void main(String[] args) {
         System.out.println("HELLO world");
+        System.out.println(FuzzySearch.tokenSetPartialRatio("certainly","it is certain"));// fuzzy search
     }
 }
