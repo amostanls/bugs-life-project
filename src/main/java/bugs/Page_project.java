3192afac8d3f24a5ca4df4cd5877621046e475c5
amostanls <tanamos@live.com>
2021-05-17 14:36:33 +0800

    authentication, main page, project page, issue page, better prompt input

---
+++ src/main/java/bugs/Page_project.java
@@ -0,0 +1,90 @@
+package bugs;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import static bugs.Page_issue.*;
+import static bugs.Page_main.*;
+import static bugs.util.*;
+
+public class Page_project {
+    public static void proj_page() throws IOException {
+        String c=null;
+        do {
+            c = ask(project.getIssues().size(), "Enter 0 to go back. \nEnter selected issue ID to check issue\nor 's' to search\nor 'c' to create issue: ", project.issueBoard());
+            if(c.equals("s")) {
+                //search issue
+                System.out.println("search");
+            }else if(c.equals("c")) {
+                //create issue
+                createIssue();
+            }else{
+                int choice = Integer.parseInt(c);
+                if(choice==0)return;
+                setIssueid(choice-1);
+                issue = (Issue) project.getIssues().get(issueid);
+                Page_issue.issue_page();
+            }
+        }while(true);
+    }
+
+    public static void createProject() {
+        //only admins can do
+        Scanner in = new Scanner(System.in);
+        System.out.println("~~~ Create New Project ~~~");
+        System.out.print("Name: ");
+        String name = in.nextLine();
+        Project current = new Project(projectenum++, name);
+        addProject(current);
+        return;
+    }
+
+    public static void addProject(Project tmp) {
+        projects.add(tmp);
+
+        len_id=Math.max(len_id, Integer.toString(projects.size()).length());
+        len_name=Math.max(len_name, tmp.getName().length());
+        len_issues=Math.max(len_issues, Integer.toString(tmp.getIssues().size()).length());
+    }
+
+    private static int len_id = 2;
+    private static int len_name = 12;
+    private static int len_issues = 6;
+    public static String projectBoard() {
+        String res = "+";
+        res += minus_plus(len_id+2);
+        res += minus_plus(len_name+2);
+        res += minus_plus(len_issues+2);
+        res += "\n";
+
+        res += "|";
+        res += centre(len_id,"ID");
+        res += centre(len_name, "Project Name");
+        res += centre(len_issues, "Issues");
+        res += "\n";
+
+        res += "+";
+        res += minus_plus(len_id+2);
+        res += minus_plus(len_name+2);
+        res += minus_plus(len_issues+2);
+        res += "\n";
+
+        for(int i=0; i<projects.size(); i++) {
+            String tmp = "|";
+            Project cur = projects.get(i);
+            tmp += right(len_id, cur.getId());
+            tmp += left(len_name, cur.getName());
+            tmp += right(len_issues, cur.getIssues().size());
+            tmp += "\n";
+            res += tmp;
+        }
+
+        res += "+";
+        res += minus_plus(len_id+2);
+        res += minus_plus(len_name+2);
+        res += minus_plus(len_issues+2);
+        res += "\n";
+
+        return  res;
+    }
+}
---
+++ src/main/java/bugs/authentication.java
@@ -0,0 +1,101 @@
+package bugs;
+
+import static bugs.Page_main.*;
+import static bugs.util.*;
+
+import java.util.Scanner;
+
+public class authentication {
+    public static int login() {
+        Scanner in = new Scanner(System.in);
+        String username, password;
+        System.out.println("~~~ Login ~~~");
+        System.out.print("Username: ");
+        username = in.nextLine();
+        System.out.print("Password: ");
+        password = in.nextLine();
+        if(username_id.containsKey(username)) {
+            int userid = (int) username_id.get(username);
+            User user = (User) users.get(userid);
+            if(password.equals(user.getPassword()))return userid;
+            else {
+                System.out.println("Incorrect password");
+                prompt_any();
+                return -1;
+            }
+        }else {
+            System.out.println("Username is not found");
+            prompt_any();
+            return -1;
+        }
+    }
+
+    public static int register() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("~~~ Register ~~~");
+        System.out.println("Please ensure your password length is at least 8 characters long.");
+        System.out.println("Name: ");
+        String name = in.nextLine();
+        System.out.print("Username: ");
+        String username = in.nextLine();
+        System.out.print("Password: ");
+        String password = in.nextLine();
+        System.out.print("Re-entered Password: ");
+        String password2 = in.nextLine();
+
+        if(username_id.containsKey(username)) {
+            System.out.println("This username has been taken");
+            prompt_any();
+            return -1;
+        }else if(!password_length(password)||!password.equals(password2)){
+            System.out.println("Passwords has less than 8 characters or passwords do no match");
+            prompt_any();
+            return -1;
+        }else {
+            //register
+            User user = new User(userenum++, username, password, name, false);
+            users.add(user);
+            return user.getUserid();
+        }
+    }
+
+    private static String SECRET = "Bugs";
+
+    public static int registerAdmin() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("~~~ Register ~~~");
+        System.out.println("Please ensure your password length is at least 8 characters long.");
+        System.out.println("Name: ");
+        String name = in.nextLine();
+        System.out.print("Username: ");
+        String username = in.nextLine();
+        System.out.print("Password: ");
+        String password = in.nextLine();
+        System.out.print("Re-entered Password: ");
+        String password2 = in.nextLine();
+        System.out.print("Secret Key: ");
+        String secret = in.nextLine();
+
+        if(username_id.containsKey(username)) {
+            System.out.println("This username has been taken");
+            prompt_any();
+            return -1;
+        }else if(!secret.equals(SECRET)){
+            System.out.println("Secret Key does not match");
+            prompt_any();
+            return -1;
+        }else if(!password_length(password)||!password.equals(password2)){
+            System.out.println("Passwords has less than 8 characters or passwords do no match");
+            prompt_any();
+            return -1;
+        }else {
+            User user = new User(userenum++, username, password, name, true);
+            users.add(user);
+            return user.getUserid();
+       }
+    }
+
+    public static boolean password_length(String s){
+        return (s.length()>=8);
+    }
+}
