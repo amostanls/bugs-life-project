7858c319429c495d33a2472651f4ca2c5c6cfd6a
Lok17 <rotabite987@gmail.com>
2021-05-29 16:51:21 +0800

    update on two factor authentication

--- src/main/java/bugs/Mail.java
+++ src/main/java/bugs/Mail.java
@@ -2,12 +2,13 @@ package bugs;
 
 import javax.mail.*;
 import javax.mail.internet.*;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 public class Mail {
     private static Random r = new Random();
 
-    public static void sendMail(String recipientMail, String title, String text) {
+    private static void sendMail(String recipientMail, String title, String text) {
         //Declare variable
         String from = "bugslifeforever4@gmail.com";
         final String username = "bugslifeforever4@gmail.com";
@@ -33,22 +34,36 @@ public class Mail {
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
+            message.setFrom(new InternetAddress(from, "Bugs Life"));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientMail));
             message.setSubject(title);
             message.setText(text);
             Transport.send(message);
             System.out.println("Sent message successfully....");
-        } catch (MessagingException e) {
+        } catch (MessagingException | UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static String authorization(String recipientMail) {
-        String title = "Welcome to bugs life. Here is your confirmation code.";
+        String title = "Welcome to bugs life. Here is your verification code.";
         String code =  String.format("%04d", r.nextInt(10000));
-        String text = "Your confirmation code is " + code + ".";
+        String text = "Your verification code is " + code + ".\n\n" +
+                "If you have not requested a verification code, someone else may be trying to access the Bugs Life account at \"+recipientMail+\". Please do not forward or give this verification code to anyone.";
+        try {
+            sendMail(recipientMail, title, text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
 
+    public static String resetPassword(String recipientMail) {
+        String title = "Bugs Life verification code.";
+        String code =  String.format("%04d", r.nextInt(10000));
+        String text = "We received a letter from your email address requesting password reset to your Bugs Life account ("+recipientMail+"). The following is your verification code.\n\n" +
+                ""+code+"\n\n" +
+                "If you have not requested a verification code, someone else may be trying to access the Bugs Life account at "+recipientMail+". Please do not forward or give this verification code to anyone.";
         try {
             sendMail(recipientMail, title, text);
         } catch (Exception e) {
