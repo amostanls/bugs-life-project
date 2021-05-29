e65152751e01e6e52924941bce26fa448fce6001
Lok17 <rotabite987@gmail.com>
2021-05-29 16:57:36 +0800

    update User

--- src/main/java/bugs/User.java
+++ src/main/java/bugs/User.java
@@ -100,4 +100,12 @@ public class User implements Serializable {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
